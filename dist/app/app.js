41212a93ae58cf620fc732383320c79038a236fa
Pratik <pratikmndh@gmail.com>
2021-10-13 23:55:49 -0400

    version2

---
+++ dist/app/app.js
@@ -0,0 +1,72 @@
+const body = document.querySelector('body');
+const hamburger = document.querySelector('#btn-hamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+const mobileLinks = document.querySelectorAll('.mobile__menu');
+const navLinks = document.querySelectorAll('.nav-links');
+const navBar = document.querySelector('.nav');
+
+// Links and buttons
+const nextSectionBtn = document.querySelector('#next-section');
+const homeBtn = document.querySelectorAll('#home-link');
+const aboutBtn = document.querySelectorAll('#about-link');
+const skillsBtn = document.querySelectorAll('#skills-link');
+const projectsBtn = document.querySelectorAll('#projects-link');
+const contactBtn = document.querySelectorAll('#contact-link');
+
+// Target sections
+const homeSection = document.querySelector('#home');
+const aboutSection = document.querySelector('#about');
+const skillsSection = document.querySelector('#skills');
+const projectsSection = document.querySelector('#projects');
+const contactSection = document.querySelector('#contact');
+
+// for mobile menu
+const toggler = function () {
+  fadeElements.forEach((el) => {
+    el.classList.toggle('has-fade');
+  });
+};
+
+hamburger.addEventListener('click', toggler);
+overlay.addEventListener('click', toggler);
+mobileLinks.forEach((link) => {
+  link.addEventListener('click', toggler);
+});
+
+// smooth scrolling
+
+const scrollTo = function (target) {
+  const targetCoords = target.getBoundingClientRect();
+  window.scrollTo({
+    left: targetCoords.left + window.pageXOffset,
+    top: targetCoords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+};
+
+nextSectionBtn.addEventListener('click', () => {
+  scrollTo(aboutSection);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    if (!e.target.dataset.id) return;
+    scrollTo(document.querySelector(`#${e.target.dataset.id}`));
+  });
+});
+
+// sticky nav bar
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  entry.isIntersecting
+    ? navBar.classList.remove('sticky')
+    : navBar.classList.add('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0.9,
+});
+
+headerObserver.observe(header);
