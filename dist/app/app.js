dfb11202d27514119dcbececdb644bd99a7ebc2b
Pratik <pratikmndh@gmail.com>
2021-10-15 15:02:58 -0400

    activate navbar link on scroll

--- dist/app/app.js
+++ dist/app/app.js
@@ -7,7 +7,6 @@ const mobileLinks = document.querySelectorAll('.mobile__menu');
 const navLinks = document.querySelectorAll('.nav-links');
 const navBar = document.querySelector('.nav');
 
-// Links and buttons
 const nextSectionBtn = document.querySelector('#next-section');
 const homeBtn = document.querySelectorAll('#home-link');
 const aboutBtn = document.querySelectorAll('#about-link');
@@ -15,17 +14,15 @@ const skillsBtn = document.querySelectorAll('#skills-link');
 const projectsBtn = document.querySelectorAll('#projects-link');
 const contactBtn = document.querySelectorAll('#contact-link');
 
-// Target sections
-const homeSection = document.querySelector('#home');
 const aboutSection = document.querySelector('#about');
-const skillsSection = document.querySelector('#skills');
-const projectsSection = document.querySelector('#projects');
-const contactSection = document.querySelector('#contact');
 
-// To reveal section on scrolling
+const SectionsToFadeIn = document.querySelectorAll('.section-fade');
 const allSections = document.querySelectorAll('.section');
+const navLinksDesktop = document.querySelectorAll('.desktop__menu');
 
-// for mobile menu
+let observe = true;
+
+// mobile menu
 const toggler = function () {
   fadeElements.forEach((el) => {
     el.classList.toggle('has-fade');
@@ -57,10 +54,21 @@ navLinks.forEach((link) => {
   link.addEventListener('click', (e) => {
     if (!e.target.dataset.id) return;
     scrollTo(document.querySelector(`#${e.target.dataset.id}`));
+
+    navLinksDesktop.forEach((link2) => {
+      link2.classList.remove('active');
+    });
+    e.target.classList.add('active');
+
+    // To deactivate Intersection observer until scroll completion
+    observe = false;
+    setTimeout(() => {
+      observe = true;
+    }, 500);
   });
 });
 
-// sticky nav bar
+// Sticky nav bar
 const stickyNav = function (entries) {
   const [entry] = entries;
   entry.isIntersecting
@@ -83,11 +91,36 @@ const revealSection = function (entries, observer) {
   observer.unobserve(entry.target);
 };
 
-const sectionObserver = new IntersectionObserver(revealSection, {
+const sectionToReveal = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0,
 });
 
+SectionsToFadeIn.forEach((section) => {
+  sectionToReveal.observe(section);
+});
+
+// Activate section items on scroll
+
+activateSection = function (entries, observer) {
+  [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  if (observe) {
+    navLinksDesktop.forEach((link) => {
+      link.classList.remove('active');
+      if (link.dataset.id === entry.target.id) {
+        link.classList.add('active');
+      }
+    });
+  }
+};
+
+const sectionToActivate = new IntersectionObserver(activateSection, {
+  root: null,
+  threshold: 1,
+});
+
 allSections.forEach((section) => {
-  sectionObserver.observe(section);
+  sectionToActivate.observe(section);
 });
